f0a176dc10251695a469c1dbfd4113e62b1f8e57
Pavel Karpov <karpovpaveldm@gmail.com>
2022-07-25 15:07:50 +0300

    fix

--- src/app/shared/interceptors/local-storage.interceptor.ts
+++ src/app/shared/interceptors/local-storage.interceptor.ts
@@ -9,25 +9,19 @@ import {
   HttpSentEvent
 } from "@angular/common/http";
 import { map, Observable, of, tap } from "rxjs";
-import { Character } from "../interfaces/character.interface";
-import { Location } from "../interfaces/location.interface";
-import { Episode } from "../interfaces/episode.interface";
-import { Filter } from "../interfaces/filter.interface";
 
 @Injectable()
 export class LocalStorageInterceptor implements HttpInterceptor{
-  constructor(private http: HttpService) { }
+  constructor() { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (localStorage.getItem(JSON.stringify(req.url))) {
-      // @ts-ignore
-      const cachedResponse = JSON.parse(localStorage.getItem(JSON.stringify(req.url)));
+      const cachedResponse = JSON.parse(localStorage.getItem(JSON.stringify(req.url)) || '');
       return of(new HttpResponse({ body: cachedResponse }));
     } else {
       return next.handle(req).pipe(
-        //@ts-ignore
-        tap((response: HttpResponse<any>) => {
-          localStorage.setItem(JSON.stringify(req.url), JSON.stringify(response.body));
+        tap((response: HttpResponse<any> | HttpEvent<any>) => {
+          localStorage.setItem(req.url, JSON.stringify((response as HttpResponse<any>).body));
         }),
       )
     }
